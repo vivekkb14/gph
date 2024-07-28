1ed6ba50c7261b40e9b7605e4d1897f668db3cdd
sandeepa.naik <sandeepa.naik@niveussolutions.com>
2024-07-28 21:02:52 +0530

    contact and gallery changes

--- my-app/src/components/NavBar/Pages/About/About.js
+++ my-app/src/components/NavBar/Pages/About/About.js
@@ -1,36 +1,236 @@
-import React from 'react'
-import pic from '../../../../assets/Doctor1.webp';
-import './About.css';
+// import React, { useState } from 'react';
+// import { Tab, Nav, Col, Row, Card } from 'react-bootstrap';
+// import './About.css';
 
-export const About = () => {
-  return (
-   <>
-    <div className='component'>
-      <div className='middleComponent'>
-        <img src={pic} className='inspection-image'></img>
-        <div className='texts'>
-          <p className='headTextComponent'>Dr Ranjith K M</p>
-          <p className='normalText'>
-            Dr Ranjith earned this Masters in Veterinary at Bangalore 
-            Veterinary College. He manages our team and makes sure all our 
-            backgrounds work together to help support our organisation.
-          </p>
-        </div>
-      </div>
-      <div className='middleComponent'>
-        <div className='texts'>
-          <p className='headTextComponent'>Dr Ranjith K M</p>
-          <p className='normalText'>
-            Dr Ranjith earned this Masters in Veterinary at Bangalore 
-            Veterinary College. He manages our team and makes sure all our 
-            backgrounds work together to help support our organisation.
-          </p>
-        </div>
-        <img src={pic} className='inspection-image'></img>
-      </div>
-    </div>
-   </>
-  )
-}
 
-export default About
+// export const About = () => {
+//   const [key, setKey] = useState('tab1');
+
+//   const tabContent = [
+//     {
+//       eventKey: 'tab1',
+//       title: 'Tab 1',
+//       image: 'https://via.placeholder.com/150',
+//       description: 'This is the description for Tab 1.'
+//     },
+//     {
+//       eventKey: 'tab2',
+//       title: 'Tab 2',
+//       image: 'https://via.placeholder.com/150',
+//       description: 'This is the description for Tab 2.'
+//     },
+//     {
+//       eventKey: 'tab3',
+//       title: 'Tab 3',
+//       image: 'https://via.placeholder.com/150',
+//       description: 'This is the description for Tab 3.'
+//     }
+//   ];
+
+//   return (
+//     <div className="container-fluid">
+//     <Tab.Container id="left-tabs-example" activeKey={key} onSelect={(k) => setKey(k)}>
+//       <Row className="tab-container">
+//         <Col sm={3} className="mb-3">
+//           <Nav variant="pills" className="flex-column">
+//             {tabContent.map((tab) => (
+//               <Nav.Item key={tab.eventKey}>
+//                 <Nav.Link eventKey={tab.eventKey}>{tab.title}</Nav.Link>
+//               </Nav.Item>
+//             ))}
+//           </Nav>
+//         </Col>
+//         <Col sm={9}>
+//           <Tab.Content>
+//             {tabContent.map((tab) => (
+//               <Tab.Pane eventKey={tab.eventKey} key={tab.eventKey}>
+//                 <Card className="horizontal-card">
+//                   <Row noGutters>
+//                     <Col md={4}>
+//                       <Card.Img variant="top" src={tab.image} className="card-img" />
+//                     </Col>
+//                     <Col md={8}>
+//                       <Card.Body>
+//                         <Card.Title>{tab.title}</Card.Title>
+//                         <Card.Text>{tab.description}</Card.Text>
+//                       </Card.Body>
+//                     </Col>
+//                   </Row>
+//                 </Card>
+//               </Tab.Pane>
+//             ))}
+//           </Tab.Content>
+//         </Col>
+//       </Row>
+//     </Tab.Container>
+//   </div>
+//   );
+// };
+
+// export default About;
+
+
+import React, { useState } from "react"; 
+import './bootstarp.css';
+import "./About.css";
+//import "@blueprintjs/core/lib/css/blueprint.css"; 
+import { Tabs, Tab } from "@blueprintjs/core"; 
+import pic1 from '../../../../assets/Doctor1.webp'
+// import { Card, CardList } from "@blueprintjs/core";
+
+const Section = ({ title, children }) => (
+  <div className="section">
+    <h2>{title}</h2>
+    {children}
+  </div>
+);
+
+const SectionCard = ({ padded, children }) => (
+  <div className={`section-card ${padded ? 'padded' : ''}`}>
+    {children}
+  </div>
+);
+
+const CardList = ({ bordered, children }) => (
+  <div className={`card-list ${bordered ? 'bordered' : ''}`}>
+    {children}
+  </div>
+);
+
+const Card = ({ children }) => (
+  <div className="card">
+    {children}
+  </div>
+);
+  
+export function About() { 
+    const [selectBool, setSelectBool] = useState(true); 
+  
+    const onHandleChange = () => { 
+        setSelectBool(!selectBool); 
+    }; 
+
+    const tabContent = [
+          {
+            eventKey: 'tab1',
+            title: 'Tab 1',
+            image: 'https://via.placeholder.com/150',
+            description: 'This is the description for Tab 1.'
+          },
+          {
+            eventKey: 'tab2',
+            title: 'Tab 2',
+            image: 'https://via.placeholder.com/150',
+            description: 'This is the description for Tab 2.'
+          },
+          {
+            eventKey: 'tab3',
+            title: 'Tab 3',
+            image: 'https://via.placeholder.com/150',
+            description: 'This is the description for Tab 3.'
+          }
+        ];
+
+    return ( 
+      <div className="contact-container">
+      <div className="contact-section">
+        <div className="App" style={{ marginLeft: 10 }}> 
+          {/* This cide is to give and option to swtich from vertical tabs to horizontal tabs
+            <h4>React.js BluePrint Component Tabs Props</h4> 
+            <p> 
+                <b style={{ marginLeft: 30 }}> 
+                    animate, vertical, large ? 
+                </b> 
+                <button 
+                    onClick={onHandleChange} 
+                    style={{  
+                        marginLeft: 10,  
+                        fontSize: 15,  
+                        padding: 10  
+                    }} 
+                > 
+                    {"" + selectBool} 
+                </button> 
+            </p>  */}
+  
+            <Tabs animate={selectBool}  
+                vertical={selectBool} large={selectBool}> 
+                <Tab id="1" title="Doctors" 
+                    panel={ <div className="contact-section">
+                      
+                                <Section title="">
+                                    <SectionCard padded={false}>
+                                        <CardList bordered={false}>
+                                            <Card className="horizontal-card">
+                                            {/* <img variant="top" src={pic1} className="card-img" /> */}
+                                            <img src={pic1} className='card-img'></img>
+                                            </Card>
+                                            <Card>
+                                                    <p className='headTextComponent'>Dr Ranjith K M</p>
+                                                    <p className='normalText'>
+                                                        Dr Ranjith earned this Masters in Veterinary at Bangalore 
+                                                        Veterinary College. He manages our team and makes sure all our 
+                                                        backgrounds work together to help support our organisation.
+                                                      </p>
+                                            </Card>
+                                            {/* ... */}
+                                        </CardList>
+                                    </SectionCard>
+                                </Section>
+                    </div>} /> 
+                <Tab id="2" title="Supporting Staff" 
+                    panel={<div className="contact-section">
+                      
+                      <Section title="">
+                          <SectionCard padded={false}>
+                              <CardList bordered={false}>
+                                  <Card className="horizontal-card">
+                                  {/* <img variant="top" src={pic1} className="card-img" /> */}
+                                  <img src={pic1} className='card-img'></img>
+                                  </Card>
+                                  <Card>
+                                          <p className='headTextComponent'>Dr Ranjith K M</p>
+                                          <p className='normalText'>
+                                              Dr Ranjith earned this Masters in Veterinary at Bangalore 
+                                              Veterinary College. He manages our team and makes sure all our 
+                                              backgrounds work together to help support our organisation.
+                                            </p>
+                                  </Card>
+                               
+                                  <Card className="horizontal-card">
+                                  {/* <img variant="top" src={pic1} className="card-img" /> */}
+                                  <img src={pic1} className='card-img'></img>
+                                  </Card>
+                                  <Card>
+                                          <p className='headTextComponent'>Dr Ranjith K M</p>
+                                          <p className='normalText'>
+                                              Dr Ranjith earned this Masters in Veterinary at Bangalore 
+                                              Veterinary College. He manages our team and makes sure all our 
+                                              backgrounds work together to help support our organisation.
+                                            </p>
+                                  </Card>
+                                  <Card className="horizontal-card">
+                                  {/* <img variant="top" src={pic1} className="card-img" /> */}
+                                  <img src={pic1} className='card-img'></img>
+                                  </Card>
+                                  <Card>
+                                          <p className='headTextComponent'>Dr Ranjith K M</p>
+                                          <p className='normalText'>
+                                              Dr Ranjith earned this Masters in Veterinary at Bangalore 
+                                              Veterinary College. He manages our team and makes sure all our 
+                                              backgrounds work together to help support our organisation.
+                                            </p>
+                                  </Card>
+                                  {/* ... */}
+                              </CardList>
+                          </SectionCard>
+                      </Section>
+          </div>} /> 
+            </Tabs> 
+        </div> 
+        </div>
+        </div>
+    ); 
+} 
+  
+export default About;
--- my-app/src/components/NavBar/Pages/Gallery/PhotoGallery/PhotoGallery.js
+++ my-app/src/components/NavBar/Pages/Gallery/PhotoGallery/PhotoGallery.js
@@ -6,43 +6,64 @@ import { Captions, Download, Fullscreen, Thumbnails, Zoom } from 'yet-another-re
 import Photos from './Photos';
 import './PhotoGallery.css';
 import "yet-another-react-lightbox/plugins/thumbnails.css";
+import pic1 from '../../../../../assets/pic1.jpg';
+import pic2 from '../../../../../assets/pic2.webp';
+import pic3 from '../../../../../assets/pic3.webp';
+import pic4 from '../../../../../assets/pic4.webp';
+import pic5 from '../../../../../assets/pic5.webp';
+import pic6 from '../../../../../assets/pic6.webp';
+import pic7 from '../../../../../assets/pic7.webp';
 
 const PhotoGallery = () => {
   const [index, setIndex] = useState(-1);
+  const Media = [
+    { type: 'img', url: pic1 }];
 
   const slides = [
     {
-      src: 'https://images.unsplash.com/photo-1682695796954-bad0d0f59ff1?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src: pic1,
       title: 'Slide Title One',
       description: 'Slide Description One\n\nthis is new line',
     },
     {
-      src: 'https://images.unsplash.com/photo-1682686581551-867e0b208bd1?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src: pic2,
       title: 'Slide Title Two',
       description: 'Slide Description Two',
     },
     {
-      src: 'https://images.unsplash.com/photo-1682695797221-8164ff1fafc9?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src: pic3,
       title: 'Slide Title Three',
       description: 'Slide Description Three',
     },
     {
-      src: 'https://images.unsplash.com/photo-1700753227268-8832c285591e?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src: pic4,
       title: 'Slide Title Nine',
       description: 'Slide Description Nine',
     },
     {
-      src: 'https://images.unsplash.com/photo-1700769025506-6c3dcb9ec9b7?q=80&w=1664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src: pic5,
       title: 'Slide Title Ten',
       description: 'Slide Description Ten',
     },
+    {
+      src: pic6,
+      title: 'Slide Title Ten',
+      description: 'Slide Description Ten',
+    },
+    {
+      src: pic7,
+      title: 'Slide Title Ten',
+      description: 'Slide Description Ten',
+    }
   ];
 
   return (
     <>
+     <div className="contact-container">
       <div className='images-container'>
         <p className='thirdComponent'>Photo Gallery</p>
       </div>
+      <div className="contact-section">
       <Photos data={slides} onClick={(currentIndex) => { setIndex(currentIndex); }} />
       
       <Lightbox
@@ -56,26 +77,8 @@ const PhotoGallery = () => {
         close={() => setIndex(-1)}
         slides={slides}
       />
-      
-      {/* <Lightbox
-    plugins={[Video]}
-    slides={[
-      {
-        type: "video",
-        width: 1280,
-        height: 720,
-        poster: "https://images.unsplash.com/photo-1700769025506-6c3dcb9ec9b7?q=80&w=1664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        sources: [
-          {
-            src: v1,
-            type: "video/mp4",
-          },
-        ],
-      },
-      // ...
-    ]}
-    // ...
-  /> */}
+      </div>
+  </div>
     </>
   );
 }
